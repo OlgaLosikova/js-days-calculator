ddae1a9db80e5f1c1c75c4352eae73203a18e6e2
ov.evdokimova <>
2023-11-06 13:22:16 +0300

    Declension of a word according to the rules

--- assets/index.js
+++ assets/index.js
@@ -2,48 +2,59 @@ const birthdayInput = document.getElementById("birthday-input");
 const checkButton = document.getElementById("check-button");
 const checkResult = document.getElementById("check-result");
 const today = new Date();
-let days = "";
 
 const numberOfDays = () => {
   const birthdayDate = new Date(birthdayInput.value);
-  const todayDay = today.getDate();
   const birthdayMonth = birthdayDate.getMonth();
+  const todayDate = today.getDate();
   const todayMonth = today.getMonth();
-  if (
-    birthdayMonth >= todayMonth &&
-    birthdayDate.getDate() >= todayDay &&
-    birthdayInput.value != ""
-  ) {
+  if (birthdayMonth >= todayMonth && birthdayDate.getDate() >= todayDate) {
     birthdayDate.setFullYear(today.getFullYear());
-  } else if (birthdayMonth <= todayMonth && birthdayInput.value != "") {
+  } else if (birthdayMonth <= todayMonth) {
     birthdayDate.setFullYear(today.getFullYear() + 1);
-    console.log(birthdayDate.getFullYear());
   }
 
   const difference = Math.ceil((birthdayDate - today) / (1000 * 60 * 60 * 24));
-  //Склонение слова "дней" по правилам русского языка
 
+  //Склонение слова "дней" по правилам русского языка
+  let days = "";
   switch (difference.toString()[difference.toString().length - 1]) {
-    case "0" || "5" || "6" || "7" || "8" || "9":
+    case "0":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
       days = "дней";
-      console.log(`${checkResult.innerText} ${days}`);
       break;
     case "1":
       days = "день";
-      console.log(`${checkResult.innerText} ${days}`);
+      if (difference == 11) {
+        days = "дней";
+      }
       break;
-    case "2" || "3" || "4":
+    case "2":
+    case "3":
+    case "4":
       days = "дня";
-      console.log(`${checkResult.innerText} ${days}`);
+      if (difference >= 12 && difference <= 14) {
+        days = "дней";
+      }
       break;
+    default:
+      days = "дней";
   }
+
   checkResult.innerText = `До вашего дня рождения осталось ${Math.abs(
     difference
   )} ${days}`;
   checkResult.style.color = "black";
+
+  //Проверка пустого значения
   if (birthdayInput.value == "") {
     checkResult.innerText = `Введите дату рождения`;
     checkResult.style.color = "red";
   }
 };
+
 checkButton.addEventListener("click", numberOfDays);
